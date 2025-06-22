95393bef92fc5c7eddf6299f03446ba0c70bbc4a
Dante07 <dante.orsetti@gmail.com>
2025-06-22 14:03:50 -0300

    Lista de Tareas Actualizada

--- react/practica/ejercitacion/src/components/ListaTareas/ListaTareas.jsx
+++ react/practica/ejercitacion/src/components/ListaTareas/ListaTareas.jsx
@@ -1,32 +1,43 @@
 import { useState } from "react";
+import "./ListaTareas.css";
 
 export const ListaTareas = () => {
-  let nombreTarea = "";
-  const listaDeTareas = [];
-  const handleMostrarTarea = (e) => {
-    nombreTarea = e.target.value;
+  const [nombreTarea, setNombreTarea] = useState("");
+  const [listaDeTareas, setListaDeTareas] = useState([]);
+
+  const handleActualizarNombre = (e) => {
+    setNombreTarea(e.target.value);
   };
+
   const handleAgregarTarea = () => {
-    listaDeTareas.push(nombreTarea);
-    console.log(listaDeTareas);
+    setListaDeTareas([...listaDeTareas, nombreTarea]);
+  };
+
+  const handleEliminarTarea = (laTarea) => {
+    const nuevaLista = listaDeTareas.filter((tarea) => tarea != laTarea);
+    setListaDeTareas(nuevaLista);
   };
 
   return (
-    <>
+    <div className="list-container">
       <h1>Lista de Tareas</h1>
       <label htmlFor="tarea">Nombre de la Tarea: </label>
       <input
         type="text"
         name="tarea"
         id="tarea"
-        onChange={handleMostrarTarea}
+        value={nombreTarea}
+        onChange={handleActualizarNombre}
       />
       <button onClick={handleAgregarTarea}>Agregar Tarea</button>
       <ul>
-        {listaDeTareas.map((tarea) => {
-          return <li>{tarea}</li>;
-        })}
+        {listaDeTareas.map((tarea, index) => (
+          <li key={index}>
+            {tarea}
+            <button onClick={() => handleEliminarTarea(tarea)}>Eliminar</button>
+          </li>
+        ))}
       </ul>
-    </>
+    </div>
   );
 };
